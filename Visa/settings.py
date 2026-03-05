00ffeb8734a2add1369f90f03368d9f0bfde5440
Bishnu <thapabishnu20@gmail.com>
2026-03-05 19:25:32 +0545

    add destination

--- Visa/settings.py
+++ Visa/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'blog',
     'clients',
     'core',
+    'destination',
     'faq',
     'inquiry',
     'page',
---
+++ destination/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
+
+# Create your models here.
+
+
+class Destination(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
+    flat=models.ImageField(upload_to='destination', help_text="471*220",default='')
+    image = models.ImageField(upload_to='destination',
+                              help_text="471*220", default='')
+    description = CKEditor5Field(config_name='extends')
+    ordering = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Destination'
+        verbose_name_plural = 'Destinations'
